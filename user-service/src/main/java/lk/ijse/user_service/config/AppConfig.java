c7cd344cc9902d33624488874313e08dd775bd95
Amidu99 <amidushamika@gmail.com>
2024-06-25 15:05:09 +0530

    update AppConfig

--- user-service/src/main/java/lk/ijse/user_service/config/AppConfig.java
+++ user-service/src/main/java/lk/ijse/user_service/config/AppConfig.java
@@ -1,5 +1,6 @@
 package lk.ijse.user_service.config;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,5 +13,10 @@ public class AppConfig {
     @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    @Bean
+    ModelMapper modelMapper(){
+        return new ModelMapper();
     }
 }
