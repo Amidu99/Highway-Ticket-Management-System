0af3e0cc961174cd02fd5d7449c342d11d4d832c
Amidu99 <amidushamika@gmail.com>
2024-06-25 15:02:15 +0530

    add UserRepo

---
+++ user-service/src/main/java/lk/ijse/user_service/repo/UserRepo.java
@@ -0,0 +1,8 @@
+package lk.ijse.user_service.repo;
+
+import lk.ijse.user_service.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepo extends MongoRepository<User, String> {
+
+}
