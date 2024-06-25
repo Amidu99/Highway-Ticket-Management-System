ad4e241f44f7b2d1ffb7d57703581ad8c4da823d
Amidu99 <amidushamika@gmail.com>
2024-06-25 15:01:27 +0530

    add UserServiceIMPL

---
+++ user-service/src/main/java/lk/ijse/user_service/service/impl/UserServiceIMPL.java
@@ -0,0 +1,11 @@
+package lk.ijse.user_service.service.impl;
+
+import lk.ijse.user_service.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceIMPL implements UserService {
+
+}
