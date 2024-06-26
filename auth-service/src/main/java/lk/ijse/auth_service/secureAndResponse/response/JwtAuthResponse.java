500d7998184ff75680600a789a55fc56c0acaaed
Amidu99 <amidushamika@gmail.com>
2024-06-26 15:56:29 +0530

    add JwtAuthResponse

---
+++ auth-service/src/main/java/lk/ijse/auth_service/secureAndResponse/response/JwtAuthResponse.java
@@ -0,0 +1,14 @@
+package lk.ijse.auth_service.secureAndResponse.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtAuthResponse {
+    private String token;
+}
