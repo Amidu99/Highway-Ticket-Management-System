7fddff80b6c688ff08598b9abd2f1dc0089e0e80
Amidu99 <amidushamika@gmail.com>
2024-06-26 15:50:07 +0530

    add auth-service

---
+++ auth-service/src/test/java/lk/ijse/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
