ca1e027ece4b21bbf6c9059ba4177922c57420be
Amidu99 <amidushamika@gmail.com>
2024-06-27 00:55:36 +0530

    add PaymentRepo

---
+++ payment-service/src/main/java/lk/ijse/payment_service/repo/PaymentRepo.java
@@ -0,0 +1,7 @@
+package lk.ijse.payment_service.repo;
+
+import lk.ijse.payment_service.model.Payment;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface PaymentRepo extends MongoRepository<Payment, String> {
+}
