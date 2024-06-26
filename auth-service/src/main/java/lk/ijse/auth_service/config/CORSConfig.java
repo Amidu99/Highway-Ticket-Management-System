0de063ea07415388a4594e95bdd46cc521de4818
Amidu99 <amidushamika@gmail.com>
2024-06-26 15:58:02 +0530

    add CORSConfig

---
+++ auth-service/src/main/java/lk/ijse/auth_service/config/CORSConfig.java
@@ -0,0 +1,18 @@
+package lk.ijse.auth_service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:63342")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
