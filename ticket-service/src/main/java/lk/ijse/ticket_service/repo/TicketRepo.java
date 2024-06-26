b3565678920495f477fba825694f6d89b0a7cc50
Amidu99 <amidushamika@gmail.com>
2024-06-26 22:50:56 +0530

    add TicketRepo

---
+++ ticket-service/src/main/java/lk/ijse/ticket_service/repo/TicketRepo.java
@@ -0,0 +1,7 @@
+package lk.ijse.ticket_service.repo;
+
+import lk.ijse.ticket_service.model.Ticket;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface TicketRepo extends MongoRepository<Ticket, String> {
+}
