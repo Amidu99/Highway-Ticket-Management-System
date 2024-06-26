601a0d19c00090f9cd66773dbc926807c98fca37
Amidu99 <amidushamika@gmail.com>
2024-06-27 00:54:32 +0530

    add TicketDTO

---
+++ payment-service/src/main/java/lk/ijse/payment_service/dto/TicketDTO.java
@@ -0,0 +1,18 @@
+package lk.ijse.payment_service.dto;
+
+import lk.ijse.payment_service.model.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class TicketDTO implements SuperDTO {
+    private String ticketNo;
+    private Date issueDate;
+    private Status status;
+    private double fineAmount;
+    private String vehicleNo;
+}
