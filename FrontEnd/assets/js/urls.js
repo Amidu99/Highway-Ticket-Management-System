8822fd5a032dead67243895bdf1e81de4463e1ad
Amidu99 <amidushamika@gmail.com>
2024-07-03 01:07:15 +0530

    update urls.js

---
+++ FrontEnd/assets/js/urls.js
@@ -0,0 +1,5 @@
+export const AuthServiceUrl = 'http://localhost:8080/api/v1/auth';
+export const PaymentServiceUrl = 'http://localhost:8080/api/v1/payment';
+export const TicketServiceUrl = 'http://localhost:8080/api/v1/ticket';
+export const UserServiceUrl = 'http://localhost:8080/api/v1/user';
+export const VehicleServiceUrl = 'http://localhost:8080/api/v1/vehicle';
