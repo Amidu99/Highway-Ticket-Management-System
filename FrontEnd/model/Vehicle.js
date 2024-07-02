8aeb71bc5058eb761d553f78ea0ecbb2396e2d6f
Amidu99 <amidushamika@gmail.com>
2024-07-03 01:14:36 +0530

    add Vehicle model

---
+++ FrontEnd/model/Vehicle.js
@@ -0,0 +1,9 @@
+export class Vehicle {
+    constructor(vehicleNo, make, type, model, ownerEmail) {
+        this.vehicleNo = vehicleNo;
+        this.make = make;
+        this.type = type;
+        this.model = model;
+        this.ownerEmail = ownerEmail;
+    }
+}
